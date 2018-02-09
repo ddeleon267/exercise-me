97ff5b2faa0ed802a8f3fc28f2d8bb83b627c945
github username <github email address>
2018-02-09 14:24:21 -0500

    Rename main page and association controller action, route, and view to 'index' from 'home'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id] = @user.id
     redirect_to controller: 'users', action: 'home'
-    #so this works but i don't want it to redirect this way
   end
 
   private
