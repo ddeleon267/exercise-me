770c3b421434692e2c7c500fef91ab308901c18d
github username <github email address>
2018-02-09 12:25:31 -0500

    Run migrations again

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180206142950) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.string "muscle_group"
+    t.string "equipment"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workout_exercises", force: :cascade do |t|
+    t.integer "sets"
+    t.integer "reps"
+    t.integer "exercise_id"
+    t.integer "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "notes"
+    t.date "date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
