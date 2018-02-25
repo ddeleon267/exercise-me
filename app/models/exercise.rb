53018fac9cea9bd2a1c030806a439d924cd5cfe9
github username <github email address>
2018-02-25 13:20:20 -0500

    Add validations to exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,8 @@
 class Exercise < ApplicationRecord
   #what attributes to i want to have? need to add validations as appropriate
+  validates_presence_of :name, :muscle_group, :email
+  validates :name, uniqueness: true
+
 
   has_many :workout_exercises
   has_many :workouts, through: :workout_exercises
