a1b19a16754761e9d0d51496f69ded192ca0d7ca
github username <github email address>
2018-02-25 16:04:50 -0500

    Fix naming convention for setting session[:user_id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    User.find_by(session[:id])
+    #can't get this to work???
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # binding.pry
+    # User.find(session[:user_id])
+  end
+
+  private
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
   end
 end
