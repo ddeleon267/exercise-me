4214a16aedba48785f2fccf5a83f853610068f43
github username <github email address>
2018-03-24 19:07:36 -0400

    Update sessions controller to use session_params helper to whitelist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(name: session_params[:name])
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to controller: 'users', action: 'home'
     else
@@ -36,6 +36,10 @@ class SessionsController < ApplicationController
 
   def auth
     request.env['omniauth.auth']
+  end
+
+  def session_params
+    params.require(:user).permit(:password, :name, :uid)
   end
 
 end
