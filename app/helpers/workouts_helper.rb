c303ef982b0745a2ada247874061c0e087d87001
github username <github email address>
2018-03-28 18:50:08 -0400

    Remove user_id_field helper from new workout form

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -3,13 +3,14 @@ module WorkoutsHelper
     workout.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
   end
 
-  def user_id_field(workout)   #am i using this somewhere? where am i using this??
-    if workout.user.nil?
-      select_tag "workout[user_id]", options_from_collection_for_select(User.all, :id, :name)
-    else
-      hidden_field_tag "workout[user_id]", workout.user_id
-    end
-  end
+  # def user_id_field(workout)   #am i using this somewhere? where am i using this??
+  #   binding.pry
+  #   if workout.user.nil?
+  #     select_tag "workout[user_id]", options_from_collection_for_select(User.all, :id, :name)
+  #   else
+  #     hidden_field_tag "workout[user_id]", workout.user_id
+  #   end
+  # end
 end
 
 # Model code isn't concerned with presentation logic. Displaying a formatted string isn't
