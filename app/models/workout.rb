344fcfaa9f29b19e4c7dac114eb392e6cee0320e
github username <github email address>
2018-04-05 12:10:24 -0400

    Done.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,7 +4,7 @@ class Workout < ApplicationRecord
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
 
-  validates_presence_of :name
+  validates_presence_of :name, :date
 
   accepts_nested_attributes_for :workout_exercises, :reject_if => proc { |attr| attr[:sets].blank? && attr[:reps].blank? && attr[:exercise_attributes][:name].blank? }
 end
