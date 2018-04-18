435f1f9db8fd61cb99e6d276d3a295319343d500
github username <github email address>
2018-04-18 09:29:23 -0400

    Add validations for description on workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,7 +4,11 @@ class Workout < ApplicationRecord
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
 
-  validates_presence_of :name, :date
+  validates_presence_of :name, :date; :description
+  validates :name, length: { minimum: 3 }
+
+  validates :description, length: { maximum: 200,
+    too_long: "%{count} characters is the maximum allowed" }
   #maybe add more validations
 
   accepts_nested_attributes_for :workout_exercises, :reject_if => proc { |attr| attr[:sets].blank? || attr[:reps].blank? || attr[:exercise_attributes][:name].blank? }
