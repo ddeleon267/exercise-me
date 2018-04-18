23606b378529502012bf3fe372b4baff81271bb6
github username <github email address>
2018-04-18 11:39:40 -0400

    Clean up exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -12,11 +12,11 @@ class ExercisesController < ApplicationController
   end
 
   def index
-     if !params[:muscle_group].blank?
-       @exercises = Exercise.muscle_group(params[:muscle_group])
-     else
-       @exercises = Exercise.all
-     end
+    if !params[:muscle_group].blank?
+      @exercises = Exercise.muscle_group(params[:muscle_group])
+    else
+      @exercises = Exercise.all
+    end
   end
 
   def show
@@ -38,8 +38,6 @@ class ExercisesController < ApplicationController
 
     def exercise_params
     	params.require(:exercise).permit(:name, :muscle_group, :equipment, :description)
-      #is the equipment attribute currently in the form? could also try implementing a view
-      #for exerises that don't require equipment? could maybe change to a boolean vs. string.
     end
 
     def set_exercise
