db67e79d34bff73f765b982b42b8e93eb613b60d
github username <github email address>
2018-06-11 14:34:35 -0400

    Add exercise serializer and associations

---
+++ app/serializers/exercise_serializer.rb
@@ -0,0 +1,6 @@
+class ExerciseSerializer < ActiveModel::Serializer
+  attributes :id, :name, :muscle_group #, :equipment, :description, :updated_at
+  #can pick and choose the attributes we want to render
+  has_many :workout_exercises
+  has_many :workouts, through: :workout_exercises
+end
