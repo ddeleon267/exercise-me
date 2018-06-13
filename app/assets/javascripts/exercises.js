0b973ab708fd3bad9bbaa24c296c3cea63b4fe0c
github username <github email address>
2018-06-13 11:45:56 -0400

    Refactor get requests into their own functions.

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -1,37 +1,43 @@
-$(() => {
-  bindClickHandlers()
-})
+$(() => bindClickHandlers())
 
 const bindClickHandlers = () => {
   $('a.all_exercises').on('click', (event) => {
      event.preventDefault()
      history.pushState(null, null, "exercises")
-     fetch(`/exercises.json`)
-     .then((response) => response.json())
-     .then((exercises) => {
-       $('#app-container').html('')
-       exercises.forEach((exercise) =>{
-         let newExercise = new Exercise(exercise)
-         let exerciseHtml = newExercise.formatIndex()
-         $("#app-container").append(exerciseHtml)
-       })
-     })
-
+     getExercises()
   })
 }
 
-  $(document).on("click", ".show_exercise", function(event) {
-    event.preventDefault()
-    $("#app-container").html("")
-    let id = $(this).attr("data-id")
-    fetch(`/exercises/${id}.json`)
-     .then((response) => response.json())
-     .then((exercise) => {
-       let newExercise = new Exercise(exercise)
-       let exerciseHtml = newExercise.formatShow()
-       $("#app-container").append(exerciseHtml)
-     })
+const getExercises = () => {
+  fetch(`/exercises.json`)
+  .then((response) => response.json())
+  .then((exercises) => {
+    $('#app-container').html('')
+    exercises.forEach((exercise) =>{
+      let newExercise = new Exercise(exercise)
+      let exerciseHtml = newExercise.formatIndex()
+      $("#app-container").append(exerciseHtml)
+    })
   })
+}
+
+const getExercise = (id) => {
+  fetch(`/exercises/${id}.json`)
+   .then((response) => response.json())
+   .then((exercise) => {
+     let newExercise = new Exercise(exercise)
+     let exerciseHtml = newExercise.formatShow()
+     $("#app-container").append(exerciseHtml)
+   })
+}
+
+$(document).on("click", ".show_exercise", function(event) {
+  event.preventDefault()
+  $("#app-container").html("")
+  let id = $(this).attr("data-id")
+  getExercise(id)
+})
+
 //constructor function
 function Exercise(exercise) {
   //can always add other attrs later
@@ -40,7 +46,7 @@ function Exercise(exercise) {
   this.muscle_group = exercise.muscle_group
 }
 
-//add prototype method
+//add prototype methods
 Exercise.prototype.formatIndex = function() {
   let exerciseHtml = `
     <a href="/exercises/${this.id}" data-id="${this.id}" class="show_exercise"><h1>${this.name}</h1></a>
@@ -48,7 +54,6 @@ Exercise.prototype.formatIndex = function() {
   return exerciseHtml
 }
 
-//add prototype method
 Exercise.prototype.formatShow = function() {
   let exerciseHtml = `
     <h3>${this.name}</ht>
